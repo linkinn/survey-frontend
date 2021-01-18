4f5ce55d262ec5614ef0f166f2109fdc1464fd35
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2021-01-18 15:31:33 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collecCoverageFrom: [
+    '<rootDir>/src/**/*.{ts.tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
